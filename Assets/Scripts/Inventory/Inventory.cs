089801617ff7f535f40e6ca81e23fd90b6ad21d2
Anthyy <42423921+Anthyy@users.noreply.github.com>
2018-10-19 16:03:42 +1100

    Implementing Inventory Items and Inventory Screen Functionality

---
+++ Assets/Scripts/Inventory/Inventory.cs
@@ -0,0 +1,96 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Inventory : MonoBehaviour
+{
+    #region Variables
+    public List<Item> inv = new List<Item>(); // List of items
+    public static bool showInv; // Show or hide inventory
+    public Item selectedItem; // The item we are interacting with
+    public int money; // How much moula we have
+
+    public Vector2 scr = Vector2.zero; // 16:9
+    public Vector2 scrollPos = Vector2.zero; // Scroll bar position 
+    #endregion
+    // Use this for initialization
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+        inv.Add(ItemData.CreateItem(0)); // And 'inv.Remove...' is to remove the item
+        inv.Add(ItemData.CreateItem(2)); 
+        inv.Add(ItemData.CreateItem(102)); 
+        inv.Add(ItemData.CreateItem(201)); 
+        inv.Add(ItemData.CreateItem(202)); 
+        inv.Add(ItemData.CreateItem(302)); 
+        inv.Add(ItemData.CreateItem(404)); // This will default to the Apple since case 404 doesn't exist and apple is the default
+
+        for (int i = 0; i < inv.Count; i++)
+        {
+            Debug.Log(inv[i].Name);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Tab))
+        {
+            if (!PauseMenu.isPaused)
+            {
+                ToggleInv();
+            }          
+        }
+    }
+    public bool ToggleInv()
+    {
+        if (showInv)
+        {
+            showInv = false;
+            Time.timeScale = 1;
+            Cursor.lockState = CursorLockMode.Locked;
+            Cursor.visible = false;
+            return (false);
+        }
+        else
+        {
+            showInv = true;
+            Time.timeScale = 0;
+            Cursor.lockState = CursorLockMode.None;
+            Cursor.visible = true;
+            return (true);
+        }
+    }
+    private void OnGUI()
+    {
+        if (!PauseMenu.isPaused) // Only display if not paused
+        {
+            if (showInv) // And it toggled on
+            {
+                if(scr.x != Screen.width/16 || scr.y != Screen.height / 9) // Update screen when needed
+                {
+                    scr.x = Screen.width / 16;
+                    scr.y = Screen.height / 9;
+                }
+                GUI.Box(new Rect(0, 0, Screen.width, Screen.height), "Inventory");
+                #region Non-scroll Inventory
+                if(inv.Count <= 35) // If we 35 or less items on the screen/zone...
+                {
+                    for (int i = 0; i < inv.Count; i++)
+                    {
+                        if(GUI.Button(new Rect(0.5f*scr.x, 0.25f*scr.y + i*(0.25f*scr.y), 3*scr.x, 0.25f*scr.y), inv[i].Name)) // show GUI button on the screen for each item
+                        {
+                            selectedItem = inv[i]; // If this item is selected set it to the selected item
+                            Debug.Log(selectedItem.Name);
+                        }
+                    }
+                }
+                #endregion
+                #region Scroll Inventory
+
+                #endregion
+            }
+        }
+    }
+}
--- Assets/Scripts/PauseMenu.cs
+++ Assets/Scripts/PauseMenu.cs
@@ -1,47 +1,54 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+[AddComponentMenu("Skyrim2.0/Menus/Pause")]
 
 public class PauseMenu : MonoBehaviour
 {
+    public static bool isPaused; // Show or hide pause menu.
+    public GameObject pauseMenu; // Allows for a scene GameObject to be selected in the inspector that will be called using this script (in this case, this will be the 'PauseMenu' GameObject Canvas elements).
 
-    public bool isPaused = false;
-    public GameObject pauseMenu;
-
-    /*void Start()
-    {
-        Time.timeScale = 1; // Game is running in realtime
-        pauseMenu.SetActive(false);
-        isPaused = false;
-    }*/
-
-
+    // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Escape))
+        if (Input.GetKeyDown(KeyCode.Escape)) // If the Esc key is pressed...
         {
-            // == checking
-            // = setting
-            if (isPaused == false)
+            if (isPaused == false) // and if the game is not paused...
             {
-                ActivateMenu();
-            }           
-            else if (isPaused) // Or isPaused == true;
+                ActivateMenu(); // call this function (activate the pause menu).
+            }
+            else if (isPaused == true) // Otherwise, if the game is paused, then...
             {
-                DeactivateMenu();
+                DeactivateMenu(); // call this function (deactivate the pause menu).
             }
         }
     }
 
-    void ActivateMenu()
+    void ActivateMenu() // Function for activating the pause menu.
     {
-        pauseMenu.SetActive(true);
-        isPaused = true;
+        isPaused = true; // Game is paused.
+        pauseMenu.SetActive(true); // Bring up the 'PauseMenu' GameObject Canvas elements.
+        Time.timeScale = 0; // Game is frozen.
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
     }
-    void DeactivateMenu()
+
+    void DeactivateMenu() // Function for deactivating the pause menu.
     {
-        pauseMenu.SetActive(false);
-        isPaused = false;
+       if(!Inventory.showInv) // Referencing the 'Inventory' script and grabbing the 'showInv' bool, then setting if that's true as a condition here (if the inventory menu is open)
+        {
+            isPaused = false; // Game is not paused.
+            pauseMenu.SetActive(false); // Close the 'PauseMenu' GameObject Canvas elements.
+            Time.timeScale = 1; // Game is running in realtime. 
+            Cursor.lockState = CursorLockMode.None;
+            Cursor.visible = true;
+        }
+        else
+        {
+            isPaused = false; // Game is not paused.
+            pauseMenu.SetActive(false); // Close the 'PauseMenu' GameObject Canvas elements.
+        }
     }
 }
 
